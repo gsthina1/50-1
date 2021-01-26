d2704273d54fe00e3ec50a50e15a80093581688f
gsthina1 <gowrisankar.thinakaran@gmail.com>
2021-01-27 00:11:07 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   name = 'Angular ' + VERSION.major;
   BindingData = [{url:'http://wallpaperheart.com/wp-content/uploads/2018/04/water-sceneries-hd.jpg', name:'The water valley'},
   {url:'http://wallpaperheart.com/wp-content/uploads/2018/04/trees-sceneries-hd.jpg', name:'Greenary favory'},
@@ -13,4 +13,7 @@ export class AppComponent  {
   {url:'http://wallpaperheart.com/wp-content/uploads/2018/04/Scenery-HD-Wallpapers-sceneries-hd.jpg', name:'Scenery'},
   {url:'http://wallpaperheart.com/wp-content/uploads/2018/04/scenery-hd-wallpaper_sceneries-hd.jpg', name:'Beautiful place'},
   {url:'http://wallpaperheart.com/wp-content/uploads/2018/04/road-sceneries-hd.jpg', name:'Dream Road'}]
+  ngOnInit(){
+    
+  }
 }
