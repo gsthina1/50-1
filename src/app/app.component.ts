94e3da2aadb573ce157a7f9f05565e9fffb1bc50
gsthina1 <gowrisankar.thinakaran@gmail.com>
2021-01-27 00:09:13 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  BindingData = [{url:'http://wallpaperheart.com/wp-content/uploads/2018/04/water-sceneries-hd.jpg', name:'The water valley'},
+  {url:'http://wallpaperheart.com/wp-content/uploads/2018/04/trees-sceneries-hd.jpg', name:'Greenary favory'},
+  {url:'http://wallpaperheart.com/wp-content/uploads/2018/04/snow-sceneries-hd.jpg', name:'Snow wow'},
+  {url:'http://wallpaperheart.com/wp-content/uploads/2018/04/Scenery-HD-Wallpapers-sceneries-hd.jpg', name:'Scenery'},
+  {url:'http://wallpaperheart.com/wp-content/uploads/2018/04/scenery-hd-wallpaper_sceneries-hd.jpg', name:'Beautiful place'},
+  {url:'http://wallpaperheart.com/wp-content/uploads/2018/04/road-sceneries-hd.jpg', name:'Dream Road'}]
 }
